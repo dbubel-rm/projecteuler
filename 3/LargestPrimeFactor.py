784f5193bbb1b05177f42e745c53f5d8ea91f72d
engineerbeard <thebeard@engineerbeard.com>
2016-06-23 16:51:14 -0700

    question 3 answer in Python

---
+++ 3/LargestPrimeFactor.py
@@ -0,0 +1,36 @@
+"""
+    Project Euler Question 3:
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+
+    Authur: thebeard@engineerbeard.com
+"""
+import math
+import time
+tStart = time.time()
+
+def fermatFactorization(n):
+    a = math.ceil(math.sqrt(n))
+    b2 = (a*a) - n
+    while not math.sqrt(b2).is_integer():
+        a+=1
+        b2 = (a*a) - n
+    b=math.sqrt(b2)
+    return [(a-b), (a+b)]
+
+def findPrimeFactors(factorList):
+    primeFactors = []
+    for n in factorList:
+        [primeFactors.append(t) for t in fermatFactorization(n)]
+    print "List of prime factors:", primeFactors
+    return max(primeFactors)
+
+def findLargestPrimeFactor(n):
+    return findPrimeFactors(fermatFactorization(n))
+
+print findLargestPrimeFactor(600851475143)
+print time.time() - tStart, "seconds elapsed"
+
+
+
+
